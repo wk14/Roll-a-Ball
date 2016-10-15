3c3d80e9279adaad3696259cd7ed268b492fef03
William Liu <liuwill14@gmail.com>
2016-10-15 17:19:55 -0400

    message

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 0.1f;
+	public Text countText;
+	public Text winText;
+	public AudioClip collideSound;
+
+	private Rigidbody rb;
+	private int count;
+
+	private AudioSource source;
+
+	public Vector3 movement{ set; get; }
+	public VirtualJoyStick joystick;
+
+
+
+	void Awake()
+	{
+		source = GetComponent<AudioSource>(); 
+	}
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		setCountText ();
+		winText.text = "";
+		speed = 20;
+	}
+
+	void Update()
+	{
+		movement = dirInput ();
+		Move ();
+	} 
+
+
+	private void Move()
+	{
+		rb.AddForce (movement * speed);
+	}
+
+
+	private Vector3 dirInput()
+	{
+		Vector3 dir = Vector3.zero;
+
+		#if UNITY_STANDALONE || UNITY_WEBPLAYER
+
+		dir.x = Input.GetAxis ("Horizontal");
+		dir.z = Input.GetAxis ("Vertical");
+
+		return dir;
+
+		#elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
+
+		dir.x = joystick.Horizontal();
+		dir.z = joystick.Vertical();
+		
+		#endif
+
+		if(dir.magnitude>1)
+		{
+			dir.Normalize();
+		}
+
+		return dir;
+	}
+
+
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			source.PlayOneShot (collideSound,1F);
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			setCountText ();
+		}
+	}
+
+
+
+	void setCountText()
+	{
+		countText.text = "Score: " + count.ToString ();
+		if (count >= 66) {
+			winText.text = "<3愛你<3<3"; 
+		}
+	}
+
+
+
+}
---
+++ Assets/Scripts/VirtualJoyStick.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class VirtualJoyStick : MonoBehaviour, IDragHandler, IPointerUpHandler,IPointerDownHandler {
+
+	private Image bgImg;
+	private Image joystickImg;
+	private Vector3 inputVec;
+
+	private void Start()
+	{
+		//declares the variables
+		bgImg = GetComponent<Image>();
+		joystickImg = transform.GetChild (0).GetComponent<Image> ();
+
+	}
+
+	// creates three evidences: OnPointerDown, OnPointerUp, OnDrag
+	public virtual void OnDrag(PointerEventData ped)
+	{
+		Vector2 pos;
+		if (RectTransformUtility.ScreenPointToLocalPointInRectangle (bgImg.rectTransform, ped.position, ped.pressEventCamera, out pos)) 
+		{
+			pos.x = (pos.x / bgImg.rectTransform.sizeDelta.x);  // getting 0~1 value 
+			pos.y = (pos.y / bgImg.rectTransform.sizeDelta.y);
+			// the above still doesn't set the center of joystick to (0,0), therefore
+			inputVec = new Vector3(pos.x*2 + 1, 0, pos.y*2 -1 );
+			inputVec = (inputVec.magnitude > 1.0f) ? inputVec.normalized : inputVec; //normalized to be within the circle
+			// Move Joystick Image
+			joystickImg.rectTransform.anchoredPosition = 
+				new Vector3 (inputVec.x * (bgImg.rectTransform.sizeDelta.x / 3), inputVec.z * (bgImg.rectTransform.sizeDelta.y / 3)); //z component is 0
+			
+
+		}
+	}
+
+	public virtual void OnPointerDown(PointerEventData ped)  
+	{
+		OnDrag (ped);
+	}
+
+	public virtual void OnPointerUp(PointerEventData ped)
+	{
+		//reset the Joystick
+		inputVec = Vector3.zero;
+		joystickImg.rectTransform.anchoredPosition = Vector3.zero;
+	}
+
+
+	public float Horizontal()
+	{
+		if (inputVec.x != 0) {
+			return inputVec.x;
+		} else {
+			return Input.GetAxis ("Horizontal");
+		}
+	}
+
+	public float Vertical()
+	{
+		if (inputVec.z != 0) {
+			return inputVec.z;
+		}else{
+			return Input.GetAxis ("Vertical");
+	}
+}
+}
