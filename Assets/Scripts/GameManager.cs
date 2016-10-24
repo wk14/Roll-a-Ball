25d177395deb63fce5ed659738ea5f012e6089cb
William Liu <liuwill14@gmail.com>
2016-10-24 01:14:39 -0400

    created Shop and GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour 
+{
+	//instance will be available 
+	private static GameManager instance;
+	public static GameManager Instance{get{return instance; }}
+
+	public int currentSkinIndex = 0; // tracking which skin is been used
+	public int currency = 0;
+	public int skinAvailability = 1;
+
+	private void Awake() // load before Start() in MainMenu
+	{
+		instance = this;
+		DontDestroyOnLoad (gameObject);
+
+		//  check: has the game been played before? if not, create it. if yes, simply load the data
+		if (PlayerPrefs.HasKey ("CurrentSkin")) { // is there any current skin saved under the registry?
+			// if true, we have a previous session
+			currentSkinIndex = PlayerPrefs.GetInt("CurrentSkin");
+			currency = PlayerPrefs.GetInt ("Currency");
+			skinAvailability = PlayerPrefs.GetInt ("SkinAvailability");
+		} 
+		else 
+		{
+			// create new game
+			Save();
+		}
+
+	}
+
+	public void Save()
+	{
+		PlayerPrefs.SetInt("CurrentSkin", currentSkinIndex);
+		PlayerPrefs.SetInt ("Currency", currency);
+		PlayerPrefs.SetInt ("SkinAvailability", skinAvailability);
+	}
+
+
+}
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,19 +5,32 @@ using System.Collections;
 
 public class MainMenu : MonoBehaviour {
 
+	private const float CAMERA_TRANSITION_SPEED = 3.0f;
+
 	public GameObject levelButtonPrefab;
 	public GameObject levelButtonContainer;
+
+	public GameObject ShopButtonPrefab;
+	public GameObject ShopItemContainer;
 
 	private Transform cameraTransform;
 	private Transform cameraDesiredLookAt;
 
+	public Text currecyText;
+
+	public Material playerMaterial;
+
 	private void Start()
 	{
+		ChangePlayerSkin (GameManager.Instance.currentSkinIndex);
+		currecyText.text = "Currency: "+GameManager.Instance.currency.ToString();
+
 		cameraTransform = Camera.main.transform;
 		//using thumbnail through the object for UI 
 		Sprite[] thumbnails = Resources.LoadAll<Sprite>("Levels");
 		foreach (Sprite thumbnail in thumbnails) 
 		{
+			// Selecting Levels
 			//instantiates container as a GameObject
 			GameObject container = Instantiate (levelButtonPrefab) as GameObject;
 			container.GetComponent<Image> ().sprite = thumbnail;
@@ -29,11 +42,32 @@ public class MainMenu : MonoBehaviour {
 			string sceneName = thumbnail.name;
 			container.GetComponent<Button> ().onClick.AddListener ( () => LoadLevel(sceneName));
 		}
+
+		// Selecting textures
+		int textureIndex = 0;
+		Sprite[] textures = Resources.LoadAll<Sprite> ("Player");
+		foreach (Sprite texture in textures) 
+		{
+			GameObject container = Instantiate (ShopButtonPrefab) as GameObject;
+			container.GetComponent<Image> ().sprite = texture;
+			container.transform.SetParent (ShopItemContainer.transform, false);
+
+			int index = textureIndex;
+			container.GetComponent<Button> ().onClick.AddListener ( () => ChangePlayerSkin (index));
+			if ((GameManager.Instance.skinAvailability & 1 << index) == 1 << index) 
+			{
+				container.transform.GetChild (0).gameObject.SetActive (false);
+			}
+			textureIndex++;
+		}
 	}
 
 	private void Update()
 	{
-		
+		if (cameraDesiredLookAt != null) // if it exists
+		{
+			cameraTransform.rotation = Quaternion.Slerp(cameraTransform.rotation, cameraDesiredLookAt.rotation, CAMERA_TRANSITION_SPEED*Time.deltaTime);
+		}
 	}
 
 
@@ -44,9 +78,40 @@ public class MainMenu : MonoBehaviour {
 
 	public void LookAtMenu(Transform menuTransform)
 	{
-		Camera.main.transform.LookAt (menuTransform.position);
+		cameraDesiredLookAt = menuTransform;
+		//Camera.main.transform.LookAt (menuTransform.position);
 	}
 
 
+	private void ChangePlayerSkin(int index)
+	{
+		if ((GameManager.Instance.skinAvailability & 1 << index) == 1 << index) { // bit flag
 
+
+			float x = (index % 4) * 0.25f;
+			float y = 0.75f - ((int)index / 4) * 0.25f;
+
+			// index starts from bottom left of the 4x4 textures, therefore we flip y to the top
+			//y = 0.75f - y;
+
+			playerMaterial.SetTextureOffset ("_MainTex", new Vector2 (x, y));
+			GameManager.Instance.currentSkinIndex = index;
+			GameManager.Instance.Save (); 
+		} 
+		else 
+		{
+			// when the player doesn't have the skin, decide to buy or not
+			int cost = 100;
+			if (GameManager.Instance.currency >= cost) 
+			{
+				GameManager.Instance.currency -= cost;
+				GameManager.Instance.skinAvailability += 1 << index;
+				GameManager.Instance.Save ();
+				currecyText.text = "Currency: "+GameManager.Instance.currency.ToString();
+
+				ShopItemContainer.transform.GetChild (index).GetChild (0).gameObject.SetActive (false);
+				ChangePlayerSkin (index);
+			}
+		}
+	}
 }
